44bfdeef6e4911f25d5555bde7fa4ae5b1a4de24
AsaBrown <asabrown50@yahoo.com>
2024-11-15 09:57:29 -0500

    Initial commit

---
+++ src/main/java/com/browna/teller_back/TellerBackApplication.java
@@ -0,0 +1,13 @@
+package com.browna.teller_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TellerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TellerBackApplication.class, args);
+	}
+
+}
